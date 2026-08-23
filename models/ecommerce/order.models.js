5d3abacc0d47b50eebc09394b7b771d8d12769fc
fazal rehman <fr7479960@gmail.com>
2026-08-23 18:03:07 +0500

    remaining stuff

--- models/ecommerce/order.models.js
+++ models/ecommerce/order.models.js
@@ -1,5 +1,49 @@
-import mongoose from "mongoose";
-
-const orderSchema = new mongoose.Schema({},{timestamps})
+import mongoose, { mongo } from "mongoose";
+const AdreesSchema = new mongoose.Schema({
+    country:{
+        type : String,
+        require : true,
+    },
+    exc_location:{
+        type : String,
+        require : true,
+    },
+    PostNum:{
+         type: number,
+    }
+})
+const quantity = new mongoose.Schema({
+    producdID:{
+      type: mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+     totalItems:{
+        type : number,
+        require : true,
+    }
+})
+const orderSchema = new mongoose.Schema({
+     user_name:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    user_email:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    adrees:{
+         type : [AdreesSchema]
+    },
+   quantity:{
+         type : [quantity]
+   },
+   priceTotal:{
+        type : number,
+        default : 0,
+   }
+   
+       
+    
+},{timestamps})
 
 const Order = mongoose.module("Order",orderSchema);
--- models/ecommerce/product.models.js
+++ models/ecommerce/product.models.js
@@ -1,5 +1,30 @@
 import mongoose from "mongoose";
 
-const user = new mongoose.Schema({},{timestamps})
+const ProductSchema = new mongoose.Schema({
+     name:{
+        type : String,
+        require : true,
+        unique:true,
+    
+    },
+    description :{
+        type : String,
+        require : true,
+       
+    },
+    brand:{
+        type : String,
+        require : true,
+        
+    },
+    price:{
+        type : Number,
+        default : 0,
+    },
+    category:[{
+       type : mongoose.Schema.Types.ObjectId,
+       ref : "Category"
+    }]
+},{timestamps})
 
-const User = mongoose.module("User",user);
+const Product = mongoose.module("Product",ProductSchema);
